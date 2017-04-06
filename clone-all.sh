705cbdb202e8efae891611796b25bf1f07d832a6
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-04-06 19:42:10 -0400

    Update.

--- clone-all.sh
+++ clone-all.sh
@@ -2,25 +2,42 @@
 #
 # Checks out all VC3 projects to ~git
 #
+# 
 
-gitdir=~/git
-mkdir -p $gitdir
-cd $gitdir
-
-vc3root="https://github.com/vc3-project"
-sdccroot="https://github.com/bnl-sdcc"
+#   PROJECT LISTS, EDIT THESE
 sdccprojects="plugin-manager"
 vc3projects="pluginmanager credible vc3-info-service vc3-master vc3-core vc3-resource-tool vc3-client"
 
+# 
+#    EDITTING BELOW SHOULD NOT BE NEEDED
+#
+gitdir=~/git
+mkdir -p $gitdir
+cd $gitdir
+vc3root="https://github.com/vc3-project"
+sdccroot="https://github.com/bnl-sdcc"
 
-for p in $sdccprojects  ; do  
-    repo="${sdccroot}/${p}.git"
-    echo $repo
-    git clone $repo
+for p in $sdccprojects  ; do
+    if [ -d "p" ] ; then
+        echo "$gitdir/$p already exists. Pulling..."
+        cd $p
+        git pull
+        cd ..
+    else
+        repo="${sdccroot}/${p}.git"
+        echo $repo
+        git clone $repo
+    fi
 done
 
-for p in $vc3projects  ; do  
-    repo="${vc3root}/${p}.git"
-    echo $repo
-    git clone $repo
+for p in $vc3projects  ; do
+    if [ -d "p" ] ; then
+        echo "$gitdir/$p already exists. Pulling..."
+        cd $p
+        git pull
+        cd ..
+    else  
+        repo="${vc3root}/${p}.git"
+        echo $repo
+        git clone $repo
 done
