b4e3b6e41a54f5247b67997b3402b23cc0e64987
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-04-25 13:13:42 -0400

    Update.

--- init-auth.sh
+++ init-auth.sh
@@ -7,7 +7,7 @@ echo "Generating inital default CA and making local host cert..."
 echo ~/bin/credible -c ~/git/credible/etc/credible.conf -d hostcert localhost
 ~/bin/credible -c ~/git/credible/etc/credible.conf -d hostcert localhost > /dev/null
 
-echo "Generating Admin VC3 User"
+echo "Generating Admin VC3Admin User"
 echo ~/bin/credible -c ~/git/credible/etc/credible.conf -d usercert VC3Admin
 ~/bin/credible -c ~/git/credible/etc/credible.conf -d usercert VC3Admin > /dev/null
 
