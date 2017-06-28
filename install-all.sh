37574faad0a9da4b783075f217d3387917202a2d
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-06-28 14:07:36 -0400

    Update.

--- install-all.sh
+++ install-all.sh
@@ -5,7 +5,7 @@
 gitdir=~/git
 
 
-pyprojects="pluginmanager credible vc3-info-service vc3-client vc3-master vc3-core vc3-resource-tool"
+pyprojects="pluginmanager credible vc3-info-service vc3-client vc3-master vc3-resource-tool"
 makeprojects="vc3-builder"
 
 for p in $pyprojects; do
