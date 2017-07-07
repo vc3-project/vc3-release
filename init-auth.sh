faca8579a4fdacce3ca9264850abc45be1f0d389
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-07-07 11:30:26 -0400

    Create ~/vc3-services area and copy certs in init-auth.

--- init-auth.sh
+++ init-auth.sh
@@ -3,10 +3,20 @@
 # Bare bones auth initialization for VC3
 
 echo "Generating inital default CA and making local host cert..."
-echo ~/bin/credible -c ~/git/credible/etc/credible.conf -d hostcert localhost
-~/bin/credible -c ~/git/credible/etc/credible.conf -d hostcert localhost > /dev/null
+echo credible -c ~/git/credible/etc/credible.conf -d hostcert localhost
+credible -c ~/git/credible/etc/credible.conf -d hostcert localhost > /dev/null
+
+echo "Creating vc3-services area for certs. Copying..."
+echo mkdir -p ~/vc3-services/etc/certs/private
+mkdir -p ~/vc3-services/etc/certs/private
+echo cp ~/var/credible/ssca/defaultca/intermediate/certs/ca-chain.cert.pem ~/vc3-services/etc/certs/
+cp ~/var/credible/ssca/defaultca/intermediate/certs/ca-chain.cert.pem ~/vc3-services/etc/certs/
+echo cp ~/var/credible/ssca/defaultca/intermediate/certs/localhost.cert.pem ~/vc3-services/etc/certs/
+cp ~/var/credible/ssca/defaultca/intermediate/certs/localhost.cert.pem ~/vc3-services/etc/certs/
+echo cp ~/var/credible/ssca/defaultca/intermediate/private/localhost.keynopw.pem ~/vc3-services/etc/certs/private/
+cp ~/var/credible/ssca/defaultca/intermediate/private/localhost.keynopw.pem ~/vc3-services/etc/certs/private/
 
 echo "Generating Admin VC3Admin User"
-echo ~/bin/credible -c ~/git/credible/etc/credible.conf -d usercert VC3Admin
-~/bin/credible -c ~/git/credible/etc/credible.conf -d usercert VC3Admin > /dev/null
+echo credible -c ~/git/credible/etc/credible.conf -d usercert VC3Admin
+credible -c ~/git/credible/etc/credible.conf -d usercert VC3Admin > /dev/null
 
